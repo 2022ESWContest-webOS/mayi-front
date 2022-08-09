06ea3ec48b4fd52771958bfe4e95cef828c5ea6e
kanghyun98 <lkh981107@gmail.com>
2022-08-09 20:43:26 +0900

    feat: add basic-video-layout for test

--- src/views/MainPanel.js
+++ src/views/MainPanel.js
@@ -1,16 +1,24 @@
-import Button from '@enact/sandstone/Button';
 import kind from '@enact/core/kind';
-import {Panel, Header} from '@enact/sandstone/Panels';
+import { Panel, Header } from '@enact/sandstone/Panels';
+
+import VideoList from '../components/VideoList';
+
+// dummy data
+const videos = [
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+];
 
 const MainPanel = kind({
-	name: 'MainPanel',
+  name: 'MainPanel',
 
-	render: (props) => (
-		<Panel {...props}>
-			<Header title="Hello world!" />
-			<Button>Click me</Button>
-		</Panel>
-	)
+  render: (props) => (
+    <Panel {...props}>
+      <VideoList videos={videos} />
+    </Panel>
+  ),
 });
 
 export default MainPanel;
