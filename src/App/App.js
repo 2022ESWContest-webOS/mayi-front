d4571ff14010f1448bed57268a36bfe3924c4314
kanghyun98 <lkh981107@gmail.com>
2022-10-05 13:33:42 +0900

    feat: add EventList page with dummy data

--- src/App/App.js
+++ src/App/App.js
@@ -4,17 +4,18 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import SignInPage from '../views/SignIn';
 import CameraListPage from '../views/CameraList';
+import EventListPage from '../views/EventList';
 
 import './attachErrorHandler';
 import './App.css';
 
 const App = () => {
-  // app
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<SignInPage />} />
         <Route path="/camera" element={<CameraListPage />} />
+        <Route path="/event" element={<EventListPage />} />
       </Routes>
     </HashRouter>
   );
